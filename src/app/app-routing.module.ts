5aa0e8945af78b26f6aad13a7302589bc7621dfc
Santos Martín García <santos.martingarca2@gmail.com>
2021-09-23 22:18:20 -0600

    CHANGE: Configuracion de rutas de la app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from "./components/layout/layout.component";
+import {BusinessListComponent} from "./pages/business/business-list/business-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, children: [
+      {path: '', redirectTo: 'business', pathMatch: 'full'},
+      {path: 'business', component: BusinessListComponent}
+    ]
+  },
+  {path: '**', redirectTo: '/business'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
