3c9bf9524c05cd1651b327ff7347f1d9203f2f32
Santos Martín García <santos.martingarca2@gmail.com>
2021-09-23 22:15:11 -0600

    CREATE: Componente Bussines-list para el listado

---
+++ src/app/pages/business/business-list/business-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../../../service/api.service";
+
+@Component({
+  selector: 'app-business-list',
+  templateUrl: './business-list.component.html',
+  styleUrls: ['./business-list.component.scss']
+})
+export class BusinessListComponent implements OnInit {
+  business: any[] = [];
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.getBussiness();
+  }
+
+  getBussiness() {
+    this.apiService.get('empresas').subscribe((response) => {
+      console.log('RESP: ', response)
+    }, error => {
+      console.log('ERROR: ',error)
+    })
+  }
+
+}
