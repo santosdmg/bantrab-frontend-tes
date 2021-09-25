77e11efef02b2d773369ee2c6f5c7b6c7ed5a51b
Santos Martín García <santos.martingarca2@gmail.com>
2021-09-24 21:19:30 -0600

    ADD: Eliminacion de empresas

--- src/app/pages/business/business-list/business-list.component.ts
+++ src/app/pages/business/business-list/business-list.component.ts
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from "../../../service/api.service";
@@ -30,6 +31,38 @@ export class BusinessListComponent implements OnInit {
 
   goToEditBusiness(id: number) {
     this.router.navigateByUrl(`business/${id}/edit`);
+  }
+
+  deleteCompany(id: string){
+    Swal.fire({
+      title: '¿Quieres eliminar la empresa?',
+      text: 'Esta acción es irreversible.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        this.apiService.delete(`empresas/${id}`).subscribe((response) => {
+          if(response) {
+            Swal.fire(
+              'Éxito',
+              'La empresa fue eliminada exitosamente.',
+              'success'
+            )
+            this.getBusiness();
+          }
+        }, error=> {
+          if(error) {
+            Swal.fire(
+              'Error',
+              'No fue posible eliminar la empresa.)',
+              'error'
+            )
+          }
+        })
+      }
+    })
   }
 
 }
